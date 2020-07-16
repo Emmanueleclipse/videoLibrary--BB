7cd55f8d04993f4668871e6e2e98fc64b12b5aa3
Emmannuel Sunday <sunasunday111@gmail.com>
2020-07-16 08:29:26 +0300

    latest commit (any name )

---
+++ app/Video.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Video extends Model
+{
+    protected $fillable = ['title', 'url', 'description', 'thumbnail_url'];
+
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class)->select('comment', 'created_at', 'video_id', 'user_id');
+    }
+
+
+}
